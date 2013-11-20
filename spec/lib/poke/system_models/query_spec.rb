d52f2237bd0cc48a870e486d7f50826dba676159
Callum Jones <callum@connected.fm>
2013-11-20 12:32:07 +0800

    Fleshing out specs

--- spec/lib/poke/system_models/query_spec.rb
+++ spec/lib/poke/system_models/query_spec.rb
@@ -2,21 +2,53 @@ require 'spec_helper'
 
 describe Poke::SystemModels::Query do
 
-  it "should be able to find the most recent statements with most recent date" do
-    time_point_a = Time.at(190)
-    time_point_b = Time.at(180)
-    time_point_c = Time.at(120)
-    time_point_d = Time.at(220)
-    query_1 = described_class.create statement: "SELECT * FROM `oracle`",  occurred_at: time_point_a
-    query_2 = described_class.create statement: "SELECT * FROM `mongodb`", occurred_at: time_point_a
-    query_3 = described_class.create statement: "SELECT * FROM `mssql`",   occurred_at: time_point_d
-    query_4 = described_class.create statement: "SELECT * FROM `raven`",   occurred_at: time_point_c
-    query_5 = described_class.create statement: "SELECT * FROM `postgre`", occurred_at: time_point_b
-    query_6 = described_class.create statement: "SELECT * FROM `sqlite`",  occurred_at: time_point_d
+  it "should require a occurred_at" do
+    inst = described_class.new
+    expect do
+      inst.save
+    end.to raise_error(Sequel::ValidationFailed)
 
-    res = described_class.most_recent_statements
-    res[0].should == time_point_d
-    res[1].should =~ ["SELECT * FROM `mssql`", "SELECT * FROM `sqlite`"]
+    inst.errors.keys.should include :occurred_at
+  end
+
+  it "should require a statement" do
+    inst = described_class.new
+    expect do
+      inst.save
+    end.to raise_error(Sequel::ValidationFailed)
+
+    inst.errors.keys.should include :statement
+  end
+
+  it "should set a statement hash on save" do
+    mock(CityHash).hash64("SELECT * FROM `fun`") { 2013 }
+    inst = described_class.new statement: "SELECT * FROM `fun`", occurred_at: Time.now
+    inst.save
+    inst.statement_hash.should == 2013
+
+    inst = described_class.find(id: inst.id)
+    inst.statement_hash.should == 2013
+  end
+
+  describe ".most_recent_statements" do
+
+    it "should be able to find the most recent statements with most recent date" do
+      time_point_a = Time.at(190)
+      time_point_b = Time.at(180)
+      time_point_c = Time.at(120)
+      time_point_d = Time.at(220)
+      query_1 = described_class.create statement: "SELECT * FROM `oracle`",  occurred_at: time_point_a
+      query_2 = described_class.create statement: "SELECT * FROM `mongodb`", occurred_at: time_point_a
+      query_3 = described_class.create statement: "SELECT * FROM `mssql`",   occurred_at: time_point_d
+      query_4 = described_class.create statement: "SELECT * FROM `raven`",   occurred_at: time_point_c
+      query_5 = described_class.create statement: "SELECT * FROM `postgre`", occurred_at: time_point_b
+      query_6 = described_class.create statement: "SELECT * FROM `sqlite`",  occurred_at: time_point_d
+
+      res = described_class.most_recent_statements
+      res[0].should == time_point_d
+      res[1].should =~ ["SELECT * FROM `mssql`", "SELECT * FROM `sqlite`"]
+    end
+
   end
 
   describe ".conditionally_create" do
