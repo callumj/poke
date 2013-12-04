44dad5f051090d5c97ec06b82daff7c221169c3d
Callum Jones <callum@callumj.com>
2013-12-04 11:04:53 +0800

    Test base of reporting

---
+++ spec/lib/poke/reporters/base_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe Poke::Reporters::Base do
+
+  it "should expose all known reporters" do
+    described_class.available_implementations.should == {"temporary_tables" => Poke::Reporters::TemporaryTables}
+  end
+
+  it "should expect subclasses to implement results" do
+    expect do
+      subject.results
+    end.to raise_error(NotImplementedError)
+  end
+
+  it "should store provided hash options" do
+    sub = described_class.new opt: :value
+    sub.options.should == {opt: :value}
+  end
+
+end
