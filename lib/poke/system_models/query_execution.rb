d3ee5568feab266307645c49c04b363dff697ef9
Callum Jones <callum@callumj.com>
2013-12-05 14:49:17 +0800

    Make use of hash helping

--- lib/poke/system_models/query_execution.rb
+++ lib/poke/system_models/query_execution.rb
@@ -1,6 +1,10 @@
 module Poke
   module SystemModels
     class QueryExecution < Sequel::Model
+
+      include Poke::Utils::QueryExtensions
+      hashed_attributes :join_method, :select_method, :index_method, :selected_index
+
       many_to_one :query, class: "Poke::SystemModels::Query"
 
       many_to_many :execution_events, class: "Poke::SystemModels::ExecutionEvent", join_table: :query_execution_events, left_key: :query_execution_id, right_key: :execution_event_id
